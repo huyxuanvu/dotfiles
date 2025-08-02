e75f5ab41e951405d8d1305f7befd71c450a87d4
vũ xuân huy <110791064+huyxuanvu@users.noreply.github.com>
2025-08-02 16:45:35 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -21,13 +21,14 @@ sudo pacman -Syu --noconfirm
 
 echo "Install dependencies...."
 
-yay -S --noconfirm \ 
-    flameshot \ rofi \ apple-fonts \ feh \ 
-    brightnessctl \ picom \ twmn-git \ xautolock \
-    ttf-jetbrains-mono-nerd \ ttf-victor-mono-nerd \ thunar \
-    gvfs \ xarchiver \ thunar-archive-plugin \ thunar-media-tags-plugin \
-    \ thunar-volman \ tumbler \ ristretto \ tmux \ fcitx5-im \ fcitx5-bamboo \ 
-    zsh \ zsh-syntax-highlighting \ zsh-autosuggestions 
+yay -S --noconfirm \
+    flameshot rofi apple-fonts feh \
+    brightnessctl picom twmn-git xautolock \
+    ttf-jetbrains-mono-nerd ttf-victor-mono-nerd thunar \
+    gvfs xarchiver thunar-archive-plugin thunar-media-tags-plugin \
+    thunar-volman tumbler ristretto tmux fcitx5-im fcitx5-bamboo \
+    zsh zsh-syntax-highlighting zsh-autosuggestions
+
 
 yay -S --noconfirm \ neovim \ xclip \ fzf \ ripgrep \ npm \ miniconda3 
 
